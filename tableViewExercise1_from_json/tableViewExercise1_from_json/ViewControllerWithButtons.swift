45c66cc30820126772924fcebaea9084e27568fd
Third Rock Techkno <trt-ios-04@Thirds-MacBook-Air.local>
2020-04-15 10:46:48 +0530

    added new view controller with button

---
+++ tableViewExercise1_from_json/tableViewExercise1_from_json/ViewControllerWithButtons.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerWithButtons.swift
+//  tableViewExercise1_from_json
+//
+//  Created by Third Rock Techkno on 15/04/20.
+//  Copyright © 2020 Third Rock Techkno. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerWithButtons: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
